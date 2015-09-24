47c3266d48b0cbc28e20c754223ad0f855515add
agathaoliveira <agatha@1stdibs.com>
2015-09-24 14:06:05 -0400

    Update read me and game logic with Dominoes rules

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
-TicTacToe
+Dominoes
 =========
 
-TicTacToe game
+Thisis the dominoes game created for my graduate class at NYU.
+
+The game can support 2-4 players. At the start of the game, players are given 7 titles picked at ramdom if playing with 2 players, or 5 titles if playing with 3 or 4 players.
+
+The basis of the games is to lay down a domino next to another domino so that the numbers (or blanks) on adjacent dominoes match each other. The game will ramdonly determine which player goes first. Then, the game will then proceed in clockwise direction.
+
+The objective of the game is to get rid of all the dominoes in your hand. Each player cannot see the dominoes owned by the other players. Whoever gets rids of all dominos first, wins. At the same time, each player tries to block the other player so he cannot make a play.
+
+Players take turns going first in each game. When a player is first, he/she may place any of his/her dominoes face up on the table to start play. The first domino played is called the "set". After the set, each domino must be placed next to a matching domino. Plays can be made on either end of a single domino and on both sides of a double domino, but not on the ends of a double.
+
+If a player does not have a play, he is blocked and then draws from the boneyard until he/she picks a domino that can be played. In a 2-player game, the last two dominoes in the boneyard may not be drawn and in a 4-player game, the very last domino in the boneyard may not be drawn.
+
+The game ends either:
+
+When one player places his/her last domino, he/she wins. The number of points awarded is the total of the dots on the unplayed dominoes of the other players.
+All players still have dominoes in their hands, but none of them can make any play. The player with the lowest number of dots at hand wins the hand and receives a score equal to the total number of dots on his opponents' unplayed dominoes.
--- src/gameLogic.ts
+++ src/gameLogic.ts
@@ -1,160 +1,225 @@
-type Board = string[][];
-interface BoardDelta {
-  row: number;
-  col: number;
+class Tile {
+  leftNumber: number;
+  rightNumber: number;
+  isDouble: boolean;
+  leftChild: Tile;
+  rightChild: Tile;
+  validForTiles: boolean[];
+
+  constructor(left: number, right: number){
+    this.leftNumber = left;
+    this.rightNumber = right;
+    this.isDouble = left === right;
+    this.validForTiles = [false, false, false, false, false, false];
+    this.validForTiles[left] = true;
+    this.validForTiles[right] = true;
+  }
 }
+
+class Board
+{
+  root: Tile;
+  leftMost: Tile;
+  rightMost: Tile;
+
+  setRoot(tile: Tile)
+  {
+    this.root = tile;
+    this.leftMost = tile;
+    this.rightMost = tile;
+  }
+}
+
+enum Play
+{
+  LEFT, RIGHT, CENTER, BUY
+}
+
+class BoardDelta {
+  tile: Tile;
+  play: Play;
+}
+
+class Player
+{
+  id: number;
+  name: string;
+  hand: Tile[];
+
+  constructor(playerId: number)
+  {
+    this.id = playerId;
+  }
+
+  addTileToHand(tile: Tile)
+  {
+    this.hand[this.hand.length] = tile;
+  }
+
+  removeTileFromHand(tile: Tile)
+  {
+    var index = this.hand.indexOf(tile, 0);
+    if (index != undefined) {
+      this.hand.splice(index, 1);
+    }
+    else
+    {
+      throw new Error("Player " + this.id + " does not have tile " + tile.leftNumber + "|" + tile.rightNumber);
+    }
+  }
+
+  getNumberOfRemainingTiles()
+  {
+    return this.hand.length;
+  }
+
+  hasTileWithNumbers(firstNumber: number, secondNumber: number)
+  {
+    for(var i = 0; i < this.hand.length; i++)
+    {
+      if(this.hand[i].validForTiles[firstNumber] || this.hand[i].validForTiles[secondNumber])
+      {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+}
+
 interface IState {
-  board: Board;
-  delta: BoardDelta;
+  board?: Board;
+  delta?: BoardDelta;
+  players?: Player[];
+  house?: Player;
 }
 
 module gameLogic {
-  export const ROWS = 3;
-  export const COLS = 3;
 
-  /** Returns the initial TicTacToe board, which is a ROWSxCOLS matrix containing ''. */
   export function getInitialBoard(): Board {
-    let board: Board = [];
-    for (let i = 0; i < ROWS; i++) {
-      board[i] = [];
-      for (let j = 0; j < COLS; j++) {
-        board[i][j] = '';
-      }
-    }
-    return board;
+      return new Board();
   }
 
-  /**
-   * Returns true if the game ended in a tie because there are no empty cells.
-   * E.g., isTie returns true for the following board:
-   *     [['X', 'O', 'X'],
-   *      ['X', 'O', 'O'],
-   *      ['O', 'X', 'X']]
-   */
-  function isTie(board: Board): boolean {
-    for (let i = 0; i < ROWS; i++) {
-      for (let j = 0; j < COLS; j++) {
-        if (board[i][j] === '') {
-          // If there is an empty cell then we do not have a tie.
-          return false;
-        }
+  function getWinner(tile: Tile, currentPlayer: Player): number
+  {
+    currentPlayer.removeTileFromHand(tile);
+    if (currentPlayer.getNumberOfRemainingTiles() === 0)
+    {
+      return currentPlayer.id;
+    }
+    return undefined;
+  }
+
+  function isTie(board: Board, players: Player[], house: Player): boolean{
+    var leftTile: Tile = board.leftMost;
+    var rightTile: Tile = board.rightMost;
+
+    if (house.hasTileWithNumbers(board.leftMost.leftNumber, board.rightMost.rightNumber))
+    {
+      return false;
+    }
+
+    for(var i = 0; i < players.length; i++)
+    {
+      if (players[i].hasTileWithNumbers(board.leftMost.leftNumber, board.rightMost.rightNumber))
+      {
+        return false;
       }
     }
-    // No empty cells, so we have a tie!
+
     return true;
   }
 
   /**
-   * Return the winner (either 'X' or 'O') or '' if there is no winner.
-   * The board is a matrix of size 3x3 containing either 'X', 'O', or ''.
-   * E.g., getWinner returns 'X' for the following board:
-   *     [['X', 'O', ''],
-   *      ['X', 'O', ''],
-   *      ['X', '', '']]
-   */
-  function getWinner(board: Board): string {
-    let boardString = '';
-    for (let i = 0; i < ROWS; i++) {
-      for (let j = 0; j < COLS; j++) {
-        let cell = board[i][j];
-        boardString += cell === '' ? ' ' : cell;
-      }
-    }
-    let win_patterns = [
-      'XXX......',
-      '...XXX...',
-      '......XXX',
-      'X..X..X..',
-      '.X..X..X.',
-      '..X..X..X',
-      'X...X...X',
-      '..X.X.X..'
-    ];
-    for (let win_pattern of win_patterns) {
-      let x_regexp = new RegExp(win_pattern);
-      let o_regexp = new RegExp(win_pattern.replace(/X/g, 'O'));
-      if (x_regexp.test(boardString)) {
-        return 'X';
-      }
-      if (o_regexp.test(boardString)) {
-        return 'O';
-      }
-    }
-    return '';
-  }
-
-  /**
-   * Returns all the possible moves for the given board and turnIndexBeforeMove.
-   * Returns an empty array if the game is over.
-   */
-  export function getPossibleMoves(board: Board, turnIndexBeforeMove: number): IMove[] {
-    let possibleMoves: IMove[] = [];
-    for (let i = 0; i < ROWS; i++) {
-      for (let j = 0; j < COLS; j++) {
-        try {
-          possibleMoves.push(createMove(board, i, j, turnIndexBeforeMove));
-        } catch (e) {
-          // The cell in that position was full.
-        }
-      }
-    }
-    return possibleMoves;
-  }
-
-  /**
-   * Returns the move that should be performed when player
-   * with index turnIndexBeforeMove makes a move in cell row X col.
-   */
+  * Returns the move that should be performed when player
+  * with index turnIndexBeforeMove makes adds a domino to the board.
+  */
   export function createMove(
-      board: Board, row: number, col: number, turnIndexBeforeMove: number): IMove {
+    board: Board, tile: Tile, play: Play, turnIndexBeforeMove: number, players: Player[], house: Player): IMove {
     if (!board) {
       // Initially (at the beginning of the match), the board in state is undefined.
       board = getInitialBoard();
     }
-    if (board[row][col] !== '') {
-      throw new Error("One can only make a move in an empty position!");
+
+    var boardAfterMove = angular.copy(board);
+    if (!board.root)
+    {
+      if (play !== Play.CENTER)
+      {
+        throw new Error("One cannot make a non center play in a board with no first tile!");
+      }
+      boardAfterMove.setRoot(tile);
+      players[turnIndexBeforeMove].removeTileFromHand(tile);
     }
-    if (getWinner(board) !== '' || isTie(board)) {
-      throw new Error("Can only make a move if the game is not over!");
+    else
+    {
+      if(!board.leftMost || !board.rightMost)
+      {
+        throw new Error("One cannot make a move in a board with no pieces on the left or right sides!");
+      }
+
+      if (Play.LEFT === play)
+      {
+        boardAfterMove.leftMost.leftChild = tile;
+        boardAfterMove.leftMost = tile;
+      }
+      else if (Play.RIGHT === play)
+      {
+        boardAfterMove.rightMost.rightChild = tile;
+        boardAfterMove.rightMost = tile;
+      }
+      else if (Play.BUY === play)
+      {
+        if (house.getNumberOfRemainingTiles() === 0)
+        {
+          throw new Error("One cannot buy from the house when it has no tiles");
+        }
+
+        var tile: Tile = house.hand[Math.floor(Math.random() * house.hand.length)];
+        house.removeTileFromHand(tile);
+        players[turnIndexBeforeMove].addTileToHand(tile);
+      }
+      else
+      {
+        throw new Error("One cannot make a center move on a board that already has a tile");
+      }
     }
-    let boardAfterMove = angular.copy(board);
-    boardAfterMove[row][col] = turnIndexBeforeMove === 0 ? 'X' : 'O';
-    let winner = getWinner(boardAfterMove);
-    let firstOperation: IOperation;
-    if (winner !== '' || isTie(boardAfterMove)) {
-      // Game over.
+
+    var winner = getWinner(tile, players[turnIndexBeforeMove]);
+    var firstOperation: IOperation;
+    if (!winner || isTie(boardAfterMove, players, house)) {
+    // Game over.
       firstOperation = {endMatch: {endMatchScores:
-        winner === 'X' ? [1, 0] : winner === 'O' ? [0, 1] : [0, 0]}};
+        winner ? [winner] : [-1]}};
     } else {
       // Game continues. Now it's the opponent's turn (the turn switches from 0 to 1 and 1 to 0).
       firstOperation = {setTurn: {turnIndex: 1 - turnIndexBeforeMove}};
     }
-    let delta: BoardDelta = {row: row, col: col};
+    var delta: BoardDelta = {tile: tile, play: play};
     return [firstOperation,
-            {set: {key: 'board', value: boardAfterMove}},
-            {set: {key: 'delta', value: delta}}];
-  }
+          {set: {key: 'board', value: boardAfterMove}},
+          {set: {key: 'delta', value: delta}}];
+}
 
   export function isMoveOk(params: IIsMoveOk): boolean {
-    let move = params.move;
-    let turnIndexBeforeMove = params.turnIndexBeforeMove;
-    let stateBeforeMove: IState = params.stateBeforeMove;
-    // The state and turn after move are not needed in TicTacToe (or in any game where all state is public).
-    //let turnIndexAfterMove = params.turnIndexAfterMove;
-    //let stateAfterMove = params.stateAfterMove;
+    var move = params.move;
+    var turnIndexBeforeMove = params.turnIndexBeforeMove;
+    var stateBeforeMove: IState = params.stateBeforeMove;
+    // The state and turn after move are not needed in dominoes
+    //var turnIndexAfterMove = params.turnIndexAfterMove;
+    //var stateAfterMove = params.stateAfterMove;
 
     // We can assume that turnIndexBeforeMove and stateBeforeMove are legal, and we need
     // to verify that move is legal.
     try {
-      // Example move:
-      // [{setTurn: {turnIndex : 1},
-      //  {set: {key: 'board', value: [['X', '', ''], ['', '', ''], ['', '', '']]}},
-      //  {set: {key: 'delta', value: {row: 0, col: 0}}}]
-      let deltaValue: BoardDelta = move[2].set.value;
-      let row = deltaValue.row;
-      let col = deltaValue.col;
-      let board = stateBeforeMove.board;
-      let expectedMove = createMove(board, row, col, turnIndexBeforeMove);
+      var deltaValue: BoardDelta = move[2].set.value;
+      var tile: Tile = deltaValue.tile;
+      var play: Play = deltaValue.play;
+      var board = stateBeforeMove.board;
+      var players: Player[] = stateBeforeMove.players;
+      var house: Player = stateBeforeMove.house;
+      var expectedMove = createMove(board, tile, play, turnIndexBeforeMove, players, house);
       if (!angular.equals(move, expectedMove)) {
         return false;
       }
@@ -163,5 +228,6 @@ module gameLogic {
       return false;
     }
     return true;
-  }
+}
+
 }
