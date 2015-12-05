569616666270e28760a37e4d38bee6af94940470
agathaoliveira <agatha@1stdibs.com>
2015-12-05 16:12:27 -0500

    update correct language

--- src/game.ts
+++ src/game.ts
@@ -734,23 +734,5 @@ angular.module('myApp', ['ngTouch', 'ui.bootstrap', 'gameServices'])
     PASS: "PASS",
     CLOSE: "Close"
   });
-  translate.setLanguage('pt',  {
-    RULES_OF_TICTACTOE: "Regras do jogo",
-    RULES_SLIDE1_TITLE: "Você sabe jogar dominó?",
-    RULES_SLIDE1: "O objetivo do jogo é colocar todas as suas pedras na mesa antes do outro jogador.",
-    RULES_SLIDE2_TITLE: "Começando a partida",
-    RULES_SLIDE2: "São distribuídas 7 peças para cada jogador e as peças restantes são guardadas no dome.",
-    RULES_SLIDE3_TITLE: "Começando a partida",
-    RULES_SLIDE3: "Começa o primeiro jogador com uma pedra dobrada. Se você não tiver uma pedra dobrada, clique em passar.",
-    RULES_SLIDE4_TITLE: "Continuando a partida",
-    RULES_SLIDE4: "O jogador seguinte deve colocar uma pedra em uma extremidade fazendo com que os pontos coincidam.",
-    RULES_SLIDE5_TITLE: "O dorme",
-    RULES_SLIDE5: "Se um jogador não puder jogar, deverá comprar tantas pedras como forem necessárias do dorme. Se não puder comprar, passará o seu turno.",
-    RULES_SLIDE6_TITLE: "Final do Jogo",
-    RULES_SLIDE6: "Ganhará o jogador que colocar todas as suas pedras na mesa. Se nenhum jogador puder colocar pedras, a partida terminará. Ganhará o jogador com menos pontos.",
-    SCORE: "Pontos",
-    PASS: "PASSAR",
-    CLOSE: "Fechar"
-  });
   game.init();
 });
